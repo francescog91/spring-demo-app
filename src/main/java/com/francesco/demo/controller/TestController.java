cd9d21a429ac8bd23697201f4702a75db2948db0
Francesco Giallatini <francescogiallatini91@gmail.com>
2023-02-05 03:23:09 +0100

    test log

--- src/main/java/com/francesco/demo/controller/TestController.java
+++ src/main/java/com/francesco/demo/controller/TestController.java
@@ -1,5 +1,7 @@
 package com.francesco.demo.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -7,9 +9,13 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("test")
 public class TestController {
+	
+	private static Logger logger = LoggerFactory.getLogger(TestController.class);
 
     @GetMapping("/hello")
     public String sayHello() {
+    	logger.info("sayHello - print message");
+    	
         return "Hello World";
     }
 }
