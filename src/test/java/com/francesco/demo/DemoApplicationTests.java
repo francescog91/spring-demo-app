ebfb2ece314a2716bce52de9b34151208050b0bf
Francesco Giallatini <francescogiallatini91@gmail.com>
2023-02-05 01:05:02 +0100

    initial commit

---
+++ src/test/java/com/francesco/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.francesco.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
